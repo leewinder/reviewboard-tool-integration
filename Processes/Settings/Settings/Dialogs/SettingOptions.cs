3c41f7bca72ebb7ceaeb46357d7e1a2664b06322
Lee Winder <lee.winder@gmail.com>
2016-04-22 20:46:44 +0100

    Added settings to shared dll and save/loaded settings in the settings dialog

--- Processes/Settings/Settings/Dialogs/SettingOptions.cs
+++ Processes/Settings/Settings/Dialogs/SettingOptions.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using RB_Tools.Shared.Authentication.Credentials;
 using RB_Tools.Shared.Authentication.Targets;
 using RB_Tools.Shared.Logging;
+using RB_Tools.Shared.Settings;
 
 namespace Settings.Dialogs
 {
@@ -13,12 +14,26 @@ namespace Settings.Dialogs
         {
             InitializeComponent();
 
+            // Load our settings
+            m_settings = RB_Tools.Shared.Settings.Settings.Load();
+            UpdateDialogOptions(m_settings);
+
             // Save our logger
             m_logger = logger;
         }
 
         // Properties
-        private readonly Logging m_logger;
+        private readonly Logging    m_logger;
+        private Options             m_settings;
+
+        //
+        // Updates the dialog with the given options
+        //
+        private void UpdateDialogOptions(Options savedSettings)
+        {
+            // Update the options
+            checkBox_Logging.Checked = savedSettings.EnableLogging;
+        }
 
         //
         // Authorise reviewboard
@@ -37,6 +52,23 @@ namespace Settings.Dialogs
             string[] serverList = RB_Tools.Shared.Server.Names.Url;
             foreach (string thisServer in serverList)
                 Credentials.Clear(thisServer, m_logger);
+        }
+
+        //
+        // Close the dialog
+        //
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //
+        // Enabled logging changed
+        //
+        private void checkBox_Logging_CheckedChanged(object sender, EventArgs e)
+        {
+            m_settings.EnableLogging = checkBox_Logging.Checked;
+            RB_Tools.Shared.Settings.Settings.Save(m_settings);
         }
     }
 }
