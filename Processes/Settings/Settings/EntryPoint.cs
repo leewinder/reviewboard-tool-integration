cddef67b033ac7c70bf5cdb90918cf2c64296a7e
Lee Winder <lee.winder@gmail.com>
2016-04-22 21:31:31 +0100

    Settings now uses the command line options for logging

--- Processes/Settings/Settings/EntryPoint.cs
+++ Processes/Settings/Settings/EntryPoint.cs
@@ -11,14 +11,19 @@ namespace Settings
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            // Enable logging
-            Logging logger = Logging.Create("Settings", Logging.Type.File, Logging.Threading.MultiThread);
+            RB_Tools.Shared.Cli.Options commandLineOptions = new RB_Tools.Shared.Cli.Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, commandLineOptions))
+            {
+                // Create our logger
+                Logging.Type loggingType = (commandLineOptions.Logging == true ? Logging.Type.File : Logging.Type.Null);
+                Logging logger = Logging.Create("Settings", loggingType, Logging.Threading.MultiThread);
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dialogs.SettingOptions(logger));
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Dialogs.SettingOptions(logger));
+            }
         }
     }
 }
