b7715a92855cf46be08d9e5c2f440705be157704
Lee Winder <lee.winder@gmail.com>
2016-04-27 19:30:55 +0100

    We now set up our own environment for builds so we don't call the VS set up files

--- Automation/Templates/Build Process.bat
+++ Automation/Templates/Build Process.bat
@@ -11,7 +11,9 @@ rem Update Nuget packages
 "%~dp0..\..\External\Nuget\nuget.exe" restore "%~dp0..\..\Processes\Shared"
 "%~dp0..\..\External\Nuget\nuget.exe" restore "%~dp0..\..\Processes\%PROCESS_NAME%"
 
+rem Set up
+call "%~dp0..\Templates\Prepare Environment.bat"
+
 rem Build the solution
-call "%VS140COMNTOOLS%\vsvars32.bat"
 msbuild /nologo "%~dp0..\..\Processes\%PROCESS_NAME%\%PROCESS_NAME%.sln" /t:Clean /p:Configuration=%CONFIG_TYPE%
 msbuild /nologo "%~dp0..\..\Processes\%PROCESS_NAME%\%PROCESS_NAME%.sln" /t:Rebuild /p:Configuration=%CONFIG_TYPE%
---
+++ Automation/Templates/Prepare Environment.bat
@@ -0,0 +1,4 @@
+
+rem Add path to MSBuild Binaries
+if exist "%ProgramFiles%\MSBuild\14.0\bin" set PATH=%ProgramFiles%\MSBuild\14.0\bin;%PATH%
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\14.0\bin;%PATH%
